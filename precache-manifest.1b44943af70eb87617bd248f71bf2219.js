80a7d617b846e116ca9445cdcc0965809b243d2d
Alex Price <alex@juggleboxmedia.co.uk>
2021-03-18 21:39:06 +0000

    Updates

---
+++ precache-manifest.1b44943af70eb87617bd248f71bf2219.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc08e2a88913e175b53464122d3a1588",
+    "url": "/MyFirstReactApp/index.html"
+  },
+  {
+    "revision": "1ddc0686c1633fbdf2d0",
+    "url": "/MyFirstReactApp/static/css/main.08e68ede.chunk.css"
+  },
+  {
+    "revision": "3052bd10a698f7083449",
+    "url": "/MyFirstReactApp/static/js/2.e2268178.chunk.js"
+  },
+  {
+    "revision": "1ddc0686c1633fbdf2d0",
+    "url": "/MyFirstReactApp/static/js/main.e584f788.chunk.js"
+  },
+  {
+    "revision": "823ac343f16c0646ad95",
+    "url": "/MyFirstReactApp/static/js/runtime~main.ad9edcd2.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/MyFirstReactApp/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/MyFirstReactApp/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
